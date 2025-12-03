4f8045beec7159176d78e2504686430eb01b2760
deltea <thcheetah777@gmail.com>
2025-12-02 21:35:46 -0800

    change some stuff in day3 part2

--- day3/part2.ts
+++ day3/part2.ts
@@ -3,28 +3,25 @@ import { readFileSync } from "fs";
 const input = readFileSync("day3/input.txt", "utf-8");
 const lines = input.split("\n").filter(Boolean);
 const batteryNum = 12;
-let result = 0;
 
+let result = 0;
 for (const line of lines) {
   let joltage = "";
-  let largestIndex = -1;
+  let largest = -1;
   let nums = line.split("");
 
   for (let j = 0; j < batteryNum; j++) {
-    nums = nums.slice(largestIndex + 1);
-    largestIndex = 0;
+    nums = nums.slice(largest + 1);
+    largest = 0;
     for (let i = 0; i <= nums.length - batteryNum + j; i++) {
-      if (+nums[i]! > +nums[largestIndex]!) {
-        largestIndex = i;
+      if (+nums[i] > +nums[largest]) {
+        largest = i;
       }
     }
-    joltage += nums[largestIndex];
-
-    console.log(joltage, nums.join(""));
+    joltage += nums[largest];
   }
 
   result += +joltage;
-  console.log(joltage);
 }
 
 console.log(result);
