2f7268dd643a590e043792c0228adf0a6983d786
deltea <thcheetah777@gmail.com>
2025-12-03 21:27:00 -0800

    holyyyyy this code is so assss

---
+++ day4/part1.ts
@@ -0,0 +1,40 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("day4/input.txt", "utf-8");
+const lines = input.split("\n").filter(Boolean);
+let result = 0;
+const dirs = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [-1, -1],
+  [1, -1],
+  [-1, 1]
+]
+
+for (let row = 0; row < lines.length; row++) {
+  for (let col = 0; col < lines[row].length; col++) {
+    if (lines[row][col] !== "@") continue;
+    let count = 0;
+    for (let i = 0; i < dirs.length; i++) {
+      const newPos = [col + dirs[i][0], row + dirs[i][1]];
+      if (
+        newPos[0] < 0 ||
+        newPos[0] >= lines[row].length ||
+        newPos[1] < 0 ||
+        newPos[1] >= lines.length
+      ) continue;
+      if (lines[newPos[1]][newPos[0]] === "@") {
+        count++;
+      }
+    }
+    if (count < 4) {
+      console.log(col, row);
+      result++;
+    }
+  }
+}
+
+console.log(result);
---
+++ day4/part2.ts
@@ -0,0 +1,47 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("day4/input.txt", "utf-8");
+let result = 0;
+let lines = input.split("\n").filter(Boolean).map(line => line.split(""));
+const dirs = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [-1, -1],
+  [1, -1],
+  [-1, 1]
+]
+
+let keepGoing = true;
+while (keepGoing) {
+  let clone = [...lines];
+  keepGoing = false;
+  for (let row = 0; row < clone.length; row++) {
+    for (let col = 0; col < clone[row].length; col++) {
+      if (clone[row][col] !== "@") continue;
+      let count = 0;
+      for (let i = 0; i < dirs.length; i++) {
+        const newPos = [col + dirs[i][0], row + dirs[i][1]];
+        if (
+          newPos[0] < 0 ||
+          newPos[0] >= clone[row].length ||
+          newPos[1] < 0 ||
+          newPos[1] >= clone.length
+        ) continue;
+        if (clone[newPos[1]][newPos[0]] === "@") {
+          count++;
+        }
+      }
+      if (count < 4) {
+        keepGoing = true;
+        clone[row][col] = ".";
+        result++;
+      }
+    }
+  }
+  lines = clone;
+}
+
+console.log(result);
